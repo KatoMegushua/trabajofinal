b49dad90993b47b4276a8d2c0e2c1981bad478f8
KatoMegushua <joshua.yoc.202303534@gmail.com>
2025-11-10 14:34:33 -0600

    Agregar estilos menu admin | Joshua Yoc

--- app/Views/menu_admin.php
+++ app/Views/menu_admin.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS (CDN) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Menu Admin</title>
 </head>
 <body class="d-flex flex-column align-items-center justify-content-center vh-100 bg-light">
@@ -27,5 +29,6 @@
    <?php
         }
    ?> 
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
