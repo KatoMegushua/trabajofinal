1b6fa4f9a0440d6171ed0510a8f2df25f19fa144
KatoMegushua <joshua.yoc.202303534@gmail.com>
2025-11-10 00:04:42 -0600

    Inicio del crud-empleados | Mostrar | Joshua Yoc

--- app/Controllers/EmpleadosController.php
+++ app/Controllers/EmpleadosController.php
@@ -6,6 +6,8 @@ use App\Models\EmpleadosModel;
 
 class EmpleadosController extends BaseController
 {
+    #inicio del crud
+    #mostrar empleados
     public function index(): string
     {
         $empleados = new EmpleadosModel();
@@ -17,54 +19,5 @@ class EmpleadosController extends BaseController
         return view('empleados',$datos);
     }
 
-    public function agregarEmpleado()
-    {
-        $empleados = new EmpleadosModel();
-
-        $datos=[
-            'codigo_autor'=>$this->request->getPost('txt_codigo'),
-            'apellido'=>$this->request->getPost('txt_apellido'),
-            'nombre'=>$this->request->getPost('txt_nombre'),
-            'nacionalidad'=>$this->request->getPost('txt_nacionalidad')
-        ];
-        //ejecuta el método insert para agregar los datos en la tabla
-        $empleados->insert($datos);
-        //ejecutamos el método index para recargar la tabla
-        return $this->index();
-    }
-
-    public function eliminarEmpleado($id)
-    {
-        
-        $empleados = new EmpleadosModel();
-        //ejecuta el método delete para eliminar los datos en la tabla
-        $empleados->delete($id);
-        //ejecutamos el método index para recargar la tabla
-        return $this->index();
-    }
     
-    public function buscarEmpleado($codigo)
-    {
-        //objeto que permite usar clase autoresmodel
-        $empleados = new EmpleadosModel();
-        //array de datos
-        //el first lo que hace es posicionarnos en el registro existente relacionado
-        $datos['datos'] = $empleados->where('codigo_autor',$codigo)->first();
-        //enviamos los datos al formulario
-        return view('form_editar_empleado',$datos);
-    }
-
-    public function modificarEmpleado(){
-        $empleados = new EmpleadosModel();
-        $datos=[
-            'apellido'=>$this->request->getPost('txt_apellido'),
-            'nombre'=>$this->request->getPost('txt_nombre'),
-            'nacionalidad'=>$this->request->getPost('txt_nacionalidad')
-        ];
-
-        $codigo = $this->request->getPost('txt_codautor');
-        $empleados->update($codigo, $datos);
-        return $this->index();
-
-    }
 }
