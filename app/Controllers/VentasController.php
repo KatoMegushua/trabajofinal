e4d73fdeb62b45362a6647a3ae039cf07afbc408
KatoMegushua <joshua.yoc.202303534@gmail.com>
2025-11-10 00:56:53 -0600

    Inicio crud-ventas | Mostrar | Joshua Yoc

--- app/Controllers/VentasController.php
+++ app/Controllers/VentasController.php
@@ -1,69 +1,22 @@
 <?php
 
 namespace App\Controllers;
-//estoy haciendo uso del archivo AutoresModel.php
-use App\Models\AutoresModel;
+//estoy haciendo uso del archivo VentasModel.php
+use App\Models\VentasModel;
 
-class AutoresController extends BaseController
+class VentasController extends BaseController
 {
+    #inicio CRUD ventas
+    #mostrar venta
     public function index(): string
     {
-        $autores = new AutoresModel();
+        $ventas = new VentasModel();
         /*utilizar el método para seleccionar todos los datos de la tabla
         'datos' es el nombre del array*/
-        $datos['datos'] = $autores->findAll();
-        //autores es una vista osea una página web a la que el 
+        $datos['datos'] = $ventas->findAll();
+        //ventas es una vista osea una página web a la que el 
         //usuario va a ingresar
-        return view('autores',$datos);
+        return view('ventas',$datos);
     }
-    public function agregarAutor()
-    {
-        $autores = new AutoresModel();
-
-        $datos=[
-            'codigo_autor'=>$this->request->getPost('txt_codigo'),
-            'apellido'=>$this->request->getPost('txt_apellido'),
-            'nombre'=>$this->request->getPost('txt_nombre'),
-            'nacionalidad'=>$this->request->getPost('txt_nacionalidad')
-        ];
-        //ejecuta el método insert para agregar los datos en la tabla
-        $autores->insert($datos);
-        //ejecutamos el método index para recargar la tabla
-        return $this->index();
-    }
-
-    public function eliminarAutor($id)
-    {
-        
-        $autores = new AutoresModel();
-        //ejecuta el método delete para eliminar los datos en la tabla
-        $autores->delete($id);
-        //ejecutamos el método index para recargar la tabla
-        return $this->index();
-    }
-    
-    public function buscarAutor($codigo)
-    {
-        //objeto que permite usar clase autoresmodel
-        $autores = new AutoresModel();
-        //array de datos
-        //el first lo que hace es posicionarnos en el registro existente relacionado
-        $datos['datos'] = $autores->where('codigo_autor',$codigo)->first();
-        //enviamos los datos al formulario
-        return view('form_editar_autor',$datos);
-    }
-
-    public function modificarAutor(){
-        $autores = new AutoresModel();
-        $datos=[
-            'apellido'=>$this->request->getPost('txt_apellido'),
-            'nombre'=>$this->request->getPost('txt_nombre'),
-            'nacionalidad'=>$this->request->getPost('txt_nacionalidad')
-        ];
-
-        $codigo = $this->request->getPost('txt_codautor');
-        $autores->update($codigo, $datos);
-        return $this->index();
-
-    }
+   
 }
