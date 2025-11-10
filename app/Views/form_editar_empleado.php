af9ed64b91566d0f910bbcaa59b461dae9fbf8bd
KatoMegushua <joshua.yoc.202303534@gmail.com>
2025-11-10 01:43:48 -0600

    rutas-empleado | Joshua Yoc | vista form editar empleado

--- app/Views/form_editar_empleado.php
+++ app/Views/form_editar_empleado.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Editar Autor</title>
+    <title>Editar Empleado</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
@@ -13,16 +13,20 @@
     <div class="container">
         <div class="row">
             <div class="col-4 col-offset-4">
-                <h1>Editar Autor</h1>
-                <form action="<?=base_url('modificar_autor')?>" method="post">
-                    <label for="txt_codautor" class="form-label">Código de autor:</label><!--readoly es para hacer que el campo sea solo de lectura-->
-                    <input type="number" class="form-control" id="txt_codautor" name="txt_codautor" value="<?=$datos['codigo_autor'];?>" readonly>
+                <h1>Editar Empleado</h1>
+                <form action="<?=base_url('modificar_empleado')?>" method="post">
+                    <label for="txt_codempleado" class="form-label">Id empleado:</label><!--readoly es para hacer que el campo sea solo de lectura-->
+                    <input type="number" class="form-control" id="txt_codempleado" name="txt_codempleado" value="<?=$datos['id_empleado'];?>" readonly>
                     <label for="txt_nombre" class="form-label">Nombre:</label>
                     <input type="text" class="form-control" id="txt_nombre" name="txt_nombre" value="<?=$datos['nombre'];?>">
                     <label for="txt_apellido" class="form-label">Apellido:</label>
                     <input type="text" class="form-control" id="txt_apellido" name="txt_apellido" value="<?=$datos['apellido'];?>">
-                    <label for="txt_nacionalidad" class="form-label">Nacionalidad:</label>
-                    <input type="text" class="form-control" id="txt_nacionalidad" name="txt_nacionalidad" value="<?=$datos['nacionalidad'];?>">
+                    <label for="txt_cargo" class="form-label">Cargo:</label>
+                    <input type="text" class="form-control" id="txt_cargo" name="txt_cargo" value="<?=$datos['cargo'];?>">
+                    <label for="txt_usuario" class="form-label">Usuario:</label>
+                    <input type="text" class="form-control" id="txt_usuario" name="txt_usuario" value="<?=$datos['usuario'];?>">
+                    <label for="txt_contra" class="form-label">Cargo:</label>
+                    <input type="text" class="form-control" id="txt_contra" name="txt_contra" value="<?=$datos['contrasena'];?>">
                     <button type="submit" class="form-control btn btn-primary mt-2">Modificar</button>
                 </form>
             </div>
