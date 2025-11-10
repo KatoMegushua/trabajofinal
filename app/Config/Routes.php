086adbe96804e248c74b3ee28ceaa78e155af5c1
KatoMegushua <joshua.yoc.202303534@gmail.com>
2025-11-10 01:56:40 -0600

    rutas-ventas | Joshua Yoc

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,16 +7,23 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-$routes->get('empleados', 'EmpleadosController::index');
-
+#Empleados
 ///('nombre_ruta','nombre_controlador::nombre_metodo')
-//autores
+$routes->get('empleados', 'EmpleadosController::index');
 $routes->post('agregar_empleado', 'EmpleadosController::agregarEmpleado');
                               //parametro                                 //$1 = es el numero de parametro que voy a utilizar
 $routes->get('eliminar_empleado/(:num)', 'EmpleadosController::eliminarEmpleado/$1');
 $routes->get('buscar_empleado/(:num)', 'EmpleadosController::buscarEmpleado/$1');
 $routes->post('modificar_empleado', 'EmpleadosController::modificarEmpleado');
 
+#Ventas
+///('nombre_ruta','nombre_controlador::nombre_metodo')
+$routes->get('ventas', 'VentasController::index');
+$routes->post('agregar_venta', 'VentasController::agregarVenta');
+                              //parametro                                 //$1 = es el numero de parametro que voy a utilizar
+$routes->get('eliminar_venta/(:num)', 'VentasController::eliminarVenta/$1');
+$routes->get('buscar_venta/(:num)', 'VentasController::buscarVenta/$1');
+$routes->post('modificar_venta', 'VentasController::modificarVenta');
 
 //usuarios controller
 $routes->post('iniciar_sesion', 'UsuariosController::index');
