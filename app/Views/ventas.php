c8b7d7684642b80813fa8531c83b30b48a1ae90f
KatoMegushua <joshua.yoc.202303534@gmail.com>
2025-11-10 14:24:57 -0600

    Validación de usuarios completa | Empleados y ventas | Joshua Yoc

--- app/Views/ventas.php
+++ app/Views/ventas.php
@@ -11,6 +11,11 @@
 </head>
 
 <body>
+    <?php
+        if (!session()->get('activa')) {
+            echo "no tiene acceso";
+        } else {   
+    ?>
     <h1>Ventas</h1>
 
     <!-- Button trigger modal -->
@@ -78,6 +83,9 @@
             ?>
         </tbody>
     </table>
+    <?php
+        }
+    ?> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
     </script>
