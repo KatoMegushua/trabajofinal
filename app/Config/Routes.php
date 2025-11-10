6f799113c498fadcccd7d6bb42353ea6591cc2e4
KatoMegushua <joshua.yoc.202303534@gmail.com>
2025-11-10 01:35:20 -0600

    rutas-empleado | Joshua Yoc

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,39 +6,17 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('autores', 'AutoresController::index');
+
 $routes->get('empleados', 'EmpleadosController::index');
-$routes->get('grados', 'GradosController::index');
-$routes->get('estados', 'EstadosController::index');
-$routes->get('editoriales', 'EditorialesController::index');
-$routes->get('prestamos', 'PrestamosController::index');
-$routes->get('estudiantes', 'EstudiantesController::index');
-$routes->get('libros', 'LibrosController::index');
 
 ///('nombre_ruta','nombre_controlador::nombre_metodo')
 //autores
-$routes->post('agregar_autor', 'AutoresController::agregarAutor');
+$routes->post('agregar_empleado', 'EmpleadosController::agregarEmpleado');
                               //parametro                                 //$1 = es el numero de parametro que voy a utilizar
-$routes->get('eliminar_autor/(:num)', 'AutoresController::eliminarAutor/$1');
-$routes->get('buscar_autor/(:num)', 'AutoresController::buscarAutor/$1');
-$routes->post('modificar_autor', 'AutoresController::modificarAutor');
-//grados
-$routes->post('agregar_grado', 'GradosController::agregarGrado');
+$routes->get('eliminar_empleado/(:num)', 'EmpleadosController::eliminarEmpleado/$1');
+$routes->get('buscar_empleado/(:num)', 'EmpleadosController::buscarEmpleado/$1');
+$routes->post('modificar_empleado', 'EmpleadosController::modificarEmpleado');
 
-//estados
-$routes->post('agregar_estado', 'EstadosController::agregarEstado');
-
-//editoriales
-$routes->post('agregar_editorial', 'EditorialesController::agregarEditorial');
-
-//prestamos
-$routes->post('agregar_prestamo', 'PrestamosController::agregarPrestamo');
-
-//estudiantes
-$routes->post('agregar_estudiante', 'EstudiantesController::agregarEstudiante');
-
-//libros
-$routes->post('agregar_libro', 'LibrosController::agregarLibro');
 
 //usuarios controller
 $routes->post('iniciar_sesion', 'UsuariosController::index');
